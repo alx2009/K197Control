53acc3aebb8039e5db5d18823cb462c139f34d9a
alx2009 <100997527+alx2009@users.noreply.github.com>
2023-10-21 16:56:14 +0200

    Completed data structure for control frame

--- src/geminiK197Control.h
+++ src/geminiK197Control.h
@@ -30,6 +30,17 @@ public:
                     
     }
 
+    /**************************************************************************/
+    /*!
+        @brief  Define the measurement unit 
+    */
+    /**************************************************************************/
+    enum K197unit {
+        Volt     = 0b00,
+        Amp      = 0b10,
+        Ohm      = 0b01,
+        reserved = 0b11,
+    };
 
     struct K197mr_byte0 {
         /*!
@@ -37,17 +48,33 @@ public:
         */
         union {
             uint8_t byte0; ///< allows access to all the flags in the
-                       ///< union as one byte as uint8_t
+                           ///< union as one byte as uint8_t
             struct {
                 uint8_t range     : 3;   ///< measurement range
                 bool    relative  : 1;   ///< relative measurement when true
                 bool    undefined : 1;   ///< unknown use, normally set to 1
                 bool    ac_dc     : 1;   ///< true if AC
-                uint8_t unit      : 2;   ///< measurement unit
+                K197unit unit     : 2;   ///< measurement unit
             };
         } __attribute__((packed)); ///<
     }; ///< Structure designed to pack a number of flags into one byte
 
+    struct K197mr_byte1 {
+        /*!
+           @brief measurement result frame byte 1
+        */
+        union {
+            uint8_t byte1; ///< allows access to all the flags in the
+                           ///< union as one byte as uint8_t
+            struct {
+                uint8_t msb       : 5;   ///< measurement Most Significant Bits
+                bool    ovrange   : 1;   ///<  when true indicates overrange 
+                bool    undefined : 1;   ///< unknown use, normally set to 0
+                bool negative     : 1;   ///< measurement unit
+            };
+
+        } __attribute__((packed)); ///<
+    }; ///< Structure designed to pack a number of flags into one byte
 
 struct K197measurement {
     /*!
@@ -57,8 +84,18 @@ struct K197measurement {
       uint8_t frameBuffer[1]; ///< allows acccess to all the flags in the
                               ///< union as one unsigned char
       struct {
-          K197mr_byte0 byte0;     
+          K197mr_byte0 byte0;   
+          K197mr_byte1 byte1;   
+          //uint16_t lsb;
+          struct {
+              uint8_t hi;
+              uint8_t lo;
+          } lsb;
       };
+
+      int32_t value;    ///< allows access to all bytes as a 32 bit signed integer value
+      uint32_t uvalue;  ///< allows access to all bytes as a 32 bit unsigned integer value
+    
     } __attribute__((packed)); ///<
   }; ///< Structure designed to pack a number of flags into one byte
 
